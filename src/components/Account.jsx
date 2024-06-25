2a95a06435216457a3d90f771d53b28d6ef42294
VISHAL13 <vishalbaraiofficial01@gmail.com>
2024-06-25 15:56:15 +0530

    Updated Mobile Navigation Bar & Accounts Page

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
-import Navbar from './Navbar';
+// import Navbar from './Navbar';
 import '../CSSFiles/Accounts.css';
 import MobileNavbar from './MobileNavbar';
+import { MdAddAPhoto } from "react-icons/md";
 
 function Account() {
   return (
     <>
     <div className='Accounts'>
-      <Navbar/>
       <div className='Left'>
         <div className='Accountlogo'>
         </div>
+ 
+<form action="/action_page.php" className='imgform'>
+  <label for="myFile" class="custom-file-upload">
+  <MdAddAPhoto />
+  <div class="tooltiptext"><h5>Change profile image</h5></div>
+  </label>
+  <input type="file" id="myFile" name="filename"/>
+  {/* <input type="submit" /> */}
+</form>
 
         <div className='MiniNav'>
           <MobileNavbar/>
@@ -21,9 +30,9 @@ function Account() {
       <div className='Right'>
         <div className='Intro'>
           <h1>Welcome to Adventurer Vishal.</h1>
-
         </div>
         <div className='Content'>
+          
 
           <div>
             
